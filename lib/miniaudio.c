d6c8e74c2af4cedac30e28997b12f780825eb0c9
Jakob Kastelic <kastelic.jakob@gmail.com>
2025-06-12 21:17:57 -0700

    enable all backends

--- lib/miniaudio.c
+++ lib/miniaudio.c
@@ -8,20 +8,6 @@
 #define MA_NO_CAPTURE            // no capture (recording) support
 #define MA_NO_ENGINE             // no engine (full engine API)
 
-#if defined(_WIN32)
-    #define MA_NO_WASAPI        0 // enable WASAPI on Windows
-    #define MA_NO_DSOUND        1
-    #define MA_NO_WINMM         1
-#elif defined(__APPLE__)
-    #define MA_NO_COREAUDIO     0 // enable CoreAudio on macOS
-#elif defined(__linux__)
-    #define MA_NO_ALSA          0 // enable ALSA on Linux
-    #define MA_NO_PULSEAUDIO    1
-    #define MA_NO_JACK          1
-#else
-    #define MA_NO_NULL          0 // fallback null backend (no real audio)
-#endif
-
 #define MINIAUDIO_IMPLEMENTATION
 
 #include "miniaudio.h"
